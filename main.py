a443fa3c96f30018980e058f8f2b6139873150df
Nelly00124m <nelly00124m@gmail.com>
2025-07-07 14:59:25 -0300

    zdfs
    Signed-off-by: Nelly00124m <nelly00124m@gmail.com>

--- main.py
+++ main.py
@@ -3,5 +3,10 @@ import pandas as pd
 
 st.title("Perguntas sobre você🐹")
 
-st.chat_input("OI")
+# Exibe o campo de entrada de chat
+mensagem = st.chat_input("Digite sua mensagem aqui...")
+
+# Verifica se o usuário enviou algo
+if mensagem:
+    st.write(f"Você escreveu: {mensagem}")
 
