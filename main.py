74c37da530488c68f969f13d47255069cbeeae84
Nelly00124m <nelly00124m@gmail.com>
2025-07-07 14:43:25 -0300

    d
    Signed-off-by: Nelly00124m <nelly00124m@gmail.com>

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ import pandas as pd
 st.title("Olá mundo")
 st.button("Rayssa")
 
-st.form("formulario_perfil"):
+st.selectbox("Gênero", ["Selecione", "Feminino", "Masculino", "Não binário", "Prefiro não dizer", "Outro"])
